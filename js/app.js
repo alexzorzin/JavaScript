f59d174ac987bcfc0479cad5cc2a9611925f4069
Alex Zorzin <87234964+alexzorzin@users.noreply.github.com>
2021-10-02 23:24:19 -0300

    Updating page

---
+++ js/app.js
@@ -0,0 +1,125 @@
+let carritoDeCompras = []
+
+const contenedorProductos = document.getElementById('contenedor-productos');
+const contenedorCarrito = document.getElementById('carrito-contenedor');
+
+const contadorCarrito = document.getElementById('contadorCarrito');
+const precioTotal = document.getElementById('precioTotal');
+const selecTipo = document.getElementById('selecTipo');
+
+selecTipo.addEventListener('change', () => {
+    console.log(selecTipo.value)
+    if (selecTipo.value == 'all') {
+        mostrarProductos(stockProductos)
+    } else {
+        mostrarProductos(stockProductos.filter(el => el.Tipo == selecTipo.value))
+    }
+})
+
+mostrarProductos(stockProductos)
+
+function mostrarProductos(array) {
+    contenedorProductos.innerHTML = '';
+    for (const producto of array) {
+        let div = document.createElement('div');
+        div.classList.add('producto');
+        div.innerHTML += `<div class="card">
+                            <div class="card-image">
+                                <img class="img-fluid" src=${producto.img}>
+                                <p class="fw-normal card-title fs-4 ms-1">${producto.nombre}</h4>
+                                <a id="boton${producto.id}" class="btn-floating icon-cart-plus"></a>
+                            </div>
+                            <div class="card-content m-1">
+                                <p>${producto.desc}</p>
+                                <p>Tipo: ${producto.Tipo}</p>
+                                <p> $${producto.Precio}</p>
+                            </div>
+                        </div> `
+        contenedorProductos.appendChild(div);
+
+        let boton = document.getElementById(`boton${producto.id}`)
+
+        boton.addEventListener('click', () => {
+            agregarAlCarrito(producto.id)
+        })
+    }
+
+}
+
+function agregarAlCarrito(id) {
+    let repetido = carritoDeCompras.find(prodR => prodR.id == id);
+    if (repetido) {
+        repetido.Cantidad = repetido.Cantidad + 1;
+        document.getElementById(`Cantidad${repetido.id}`).innerHTML = `<p id="Cantidad${repetido.id}">Cantidad: ${repetido.Cantidad}</p>`
+        actualizarCarrito()
+    } else {
+        let productoAgregar = stockProductos.find(prod => prod.id == id);
+        carritoDeCompras.push(productoAgregar);
+        
+
+        productoAgregar.Cantidad = 1;
+        actualizarCarrito()
+        let div = document.createElement('div');
+        div.classList.add('productoEnCarrito');
+        div.innerHTML = `<p>${productoAgregar.nombre}</p>
+                        <p>Precio: ${productoAgregar.Precio}</p>
+                        <p id="Cantidad${productoAgregar.id}">Cantidad: ${productoAgregar.Cantidad}</p>
+                        <p>Tipo: ${productoAgregar.Tipo}</p>
+                        <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`
+        contenedorCarrito.appendChild(div);
+
+        let botonEliminar = document.getElementById(`eliminar${productoAgregar.id}`)
+
+        botonEliminar.addEventListener('click', () => {
+            botonEliminar.parentElement.remove()
+            carritoDeCompras = carritoDeCompras.filter(prodE => prodE.id != productoAgregar.id)
+            actualizarCarrito()
+        })
+    }
+}
+
+function actualizarCarrito() {
+    contadorCarrito.innerText = carritoDeCompras.reduce((acc, el) => acc + el.Cantidad, 0);
+    precioTotal.innerText = carritoDeCompras.reduce((acc, el) => acc + (el.Precio * el.Cantidad), 0)
+    guardarLocal()
+}
+
+function guardarLocal(){
+    localStorage.setItem("productos", JSON.stringify(carritoDeCompras))
+}
+
+function obtenerLocal(){
+    let carritoAlmacenado = JSON.parse(localStorage.getItem("productos")); 
+
+    if(carritoAlmacenado){
+        carritoAlmacenado.forEach(el => {
+            carritoDeCompras.push(el)
+            actualizarCarrito()
+
+            let div = document.createElement("div");
+            div.classList.add('productoEnCarrito');
+            div.innerHTML += `<p>${el.nombre}</p>
+            <p>Precio: ${el.Precio}</p>
+            <p id="Cantidad${el.id}">Cantidad: ${el.Cantidad}</p>
+            <p>Tipo: ${el.Tipo}</p>
+            <button id="eliminar${el.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>`
+        
+            contenedorCarrito.appendChild(div);
+            
+            let botonEliminar = document.getElementById (`eliminar${el.id}`);
+        
+            botonEliminar.addEventListener ("click", () => {
+                botonEliminar.parentElement.remove();
+                carritoDeCompras = carritoDeCompras.filter(prodEliminado => prodEliminado.id != el.id);
+                actualizarCarrito();
+                Toastify({
+                    text: "Producto Eliminado",
+                    backgroundColor: "red",
+                    className: "info",
+                  }).showToast();
+            })
+        });
+    }
+}
+
+obtenerLocal();
---
+++ js/stock.js
@@ -0,0 +1,9 @@
+let stockProductos = [
+    {id: 1, nombre: "Photoshop", Tipo: "Programas", desc: "Con este programa lograrás editar todo tipo de imagén para tu negocio personal o simplemente por gusto.", Precio: 500, img: './assets/img/photoshop.jpg', Cantidad:1},
+    {id: 2, nombre: "FIFA 22", Tipo: "Juegos", desc: "Juega con más de 17 000 jugadores, más de 700 equipos, más de 90 estadios y más de 30 ligas de todo el mundo.", Precio: 5140, img: './assets/img/fifa22.jfif', Cantidad:1},
+    {id: 3, nombre: "Paquete Office", Tipo: "Programas", desc: "Este paquete incluye las principales aplicaciones que se usan en espacios de trabajo como oficinas o escuelas.", Precio: 5000, img: './assets/img/office.png', Cantidad:1},
+    {id: 4, nombre: "Rust", Tipo: "Juegos", desc: "Juega Rust, un juego donde tienes que sobrevivir. Cuidate de tu entorno y de los otros sobrevivientes. Haz lo necesario para vivir otra noche.", Precio: 2050, img: './assets/img/rust.jpg', Cantidad:1},
+    {id: 5, nombre: "Sony VEGAS Pro", Tipo: "Programas", desc: "Este programa sirve para editar todo tipo de contenido audiovisual para subirlo a una red social, utilizarlo en un entorno laboral, o para otros lugares y momentos.", Precio: 940, img: './assets/img/sonyvegas.png', Cantidad:1},
+    {id: 6, nombre: "Rainbow Six Siege", Tipo: "Juegos", desc: "Domina la destrucción y la tecnología en Rainbow Six Siege. Tén un combate cercano altamente letal, con táctica, juego en equipo y acción en todo momento.", Precio: 780, img: './assets/img/rainbow6.jpg', Cantidad:1},
+]
+
